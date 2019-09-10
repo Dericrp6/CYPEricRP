024472d864da6c8162c235449290086af390eb24
Dericrp6 <ericpichardo5140@gmail.com>
2019-09-10 16:44:05 -0500

    dos archivos nuevos

---
+++ adios.py
@@ -0,0 +1,5 @@
+print("adios mundo cruel")
+i.email "ericpichardo5140@gmail.com"
+
+i.email "ericpichardo5140@gmail.com"
+
---
+++ resucite.py
@@ -0,0 +1,3 @@
+print("no estaba muerto, estaba en el bar shine")
+e.email "ericpichardo5140@gmail.com"
+
