399033eae401906583d9a0322ebb03123ab9752f
Dericrp6 <ericpichardo5140@gmail.com>
2019-09-17 17:13:43 -0500

    trabajo ejersicio12.py

---
+++ libro/ejemplo12.py
@@ -0,0 +1,8 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:/Users/usuario/CYPEricRP/libro/ejemplo12.py ===========
+introduce un valor entero positivo:2
+introduce otro valor entero posivito2
+2.5198420997897464
+>>> 
