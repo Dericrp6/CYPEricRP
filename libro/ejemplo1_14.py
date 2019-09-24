493f5041cba5182ed081c345979cabb6cb8bd04f
Dericrp6 <ericpichardo5140@gmail.com>
2019-09-24 00:50:27 -0500

    ejersicios de tarea resueltos

---
+++ libro/ejemplo1_14.py
@@ -0,0 +1,11 @@
+print("CUADRADO_CUBO")
+
+NUM = int(input("Dame un numero entero: "))
+
+CUA = NUM * NUM
+CUB = NUM ** 3
+
+print(f"El cuadrado de {NUM} es {CUA}")
+print(f"El cubo de {NUM} es {CUB}")
+
+print("gracias por haber participado C:")
---
+++ libro/ejemplo1_15.py
@@ -0,0 +1,9 @@
+print("PERMIETRO_SUPERFICIE_RECTANGULO")
+
+BASE = float(input("dame la base del rectangulo: "))
+ALTU = float(input("dame la altura del rectangulo: "))
+
+SUP = (BASE * ALTU)
+PER = 2 * (BASE + ALTU)
+
+print(f"el perimetro de este triangulo es { PER } dado que su superficie es { PER } ")
