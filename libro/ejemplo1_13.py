68e69c809f163674bfab250be5d5a1f1f6392bf4
Dericrp6 <ericpichardo5140@gmail.com>
2019-09-24 22:55:16 -0500

    repositorio modificado

---
+++ libro/ejemplo1_13.py
@@ -0,0 +1,14 @@
+print("PROMEDIO_CALIFICACION")
+MAT = int(input("Dame la calificacion de las materias")
+
+CAL1 = 6
+CAL2 = 9
+CAL3 = 10
+CAL4 = 7
+cAL5 = 8
+
+
+PRO = (CAL1 + CAL2 + CAL3 + CAL4 + CAL5)/5
+
+MAT = print(f"Las calificaciones de las materias son {CAL1}, {CAL2}, {CAL3}, {CAL4} y {CAL5} asi que el: ")
+PRO = print("promedio del semestre es {PRO}")
