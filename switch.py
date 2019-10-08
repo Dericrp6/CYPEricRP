b2536529350a610b272521bc5e9e798690b8b727
Dericrp6 <ericpichardo5140@gmail.com>
2019-10-08 17:41:07 -0500

    ejemplos en clase resueltos

---
+++ switch.py
@@ -0,0 +1,19 @@
+dia = int(input("Introduce un dia (1-5): "))
+if dia == 1 :
+    print("Lunes")
+elif dia == 2: 
+    print("Martes")
+elif dia == 3 :
+    print("Miercoles")
+elif dia == 4:
+    print("jueves")
+elif dia == 5 :
+    print("Viernes")
+else:
+    print("Opcion no valida")
+
+
+print("fin del progrma")
+
+
+
