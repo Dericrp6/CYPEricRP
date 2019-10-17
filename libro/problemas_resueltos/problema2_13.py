fd4c54f0a27763ba63aa4a1c0f108b3b3785d314
Dericrp6 <ericpichardo5140@gmail.com>
2019-10-17 00:14:15 -0500

    tarea 16/10/19

---
+++ libro/problemas_resueltos/problema2_13.py
@@ -0,0 +1,17 @@
+MAT = int(input("Caule es su matricula?: "))
+CARR = int(input("En que carrera esta inscrito?: "))
+SEM = int(input("Semestre: "))
+PROM = int(input("Su promedio: "))
+
+if CARR:
+    SEM >= 6 and PROM >= 8.8
+    print(f"Su matricula {MAT} inscrito en {CARR} fue Aceptado")
+
+    SEM > 6 and PROM > 8.5
+    print(f"Su matricula {MAT} inscrito en {CARR} fue Aceptado")
+
+    SEM > 5 and PROM > 8.5
+    print(f"Su matricula {MAT} inscrito en {CARR} fue Aceptado")
+
+
+
