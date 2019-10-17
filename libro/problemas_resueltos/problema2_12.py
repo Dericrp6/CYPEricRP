856452ebaa5dbf1ba449690c320a331022ed7fc6
Dericrp6 <ericpichardo5140@gmail.com>
2019-10-16 23:58:41 -0500

    tarea 16/10/19

--- libro/problemas_resueltos/problema2_12.py
+++ libro/problemas_resueltos/problema2_12.py
@@ -1,1 +1,15 @@
-eeee
+SUE = int(input("Cual es su sueldo?: "))
+CATE = int(input("Cual es su categoria como trabajador?: "))
+HE = int(input("Cuantas horas extras trabajo?: "))
+
+if CATE:
+    PHE = 0
+
+elif HE > 30:
+    NSUE = SUE + 30*PHE
+
+else:
+    NSUE = SUE + HE*PHE
+
+print(f"Su nuevo sueldo es de {NSUE}")
+
