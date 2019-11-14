5f2ca3b43f4a0edc9f33f555fdf62d042ef60a00
Dericrp6 <ericpichardo5140@gmail.com>
2019-11-14 14:32:29 -0600

    tarea

--- letrero_colores.py
+++ letrero_colores.py
@@ -1,6 +1,24 @@
+
+
+from asciistuff import Banner
+
+from colorutils import Color
+c = Color ((255,255,255))
+
+for i in range (10,0,-1):
+    print(i , "..." )
+    
+print(Banner("Universidad Nacional Autonoma de Mexico!!"))
+
+
+
+
+
+"""
 from asciistuff import Banner
 from colorutils import Color
 
 c = Color ((255,255,255))
 
 print(Banner("Universidad Nacional Autonoma de Mexico")
+"""
